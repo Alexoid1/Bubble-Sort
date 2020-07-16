774369df318c7805aca95b85fb9a0ec07c6e248c
devPipe <felipeangulo2442@gmail.com>
2020-07-16 12:15:27 -0500

    Fix linters - FIxe method size and metrics/AbcSize

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,11 +1,9 @@
-def bubble_sort(arra)
-  my_array = arra
-  n = arra.length - 1
-  puts n
+# rubocop:disable Metrics/AbcSize
+
+def bubble_sort(my_array)
   loop do
-    my_array.map.with_index do |num, i|
-      puts "value #{num} the index is #{i}"
-      return my_array unless i != n
+    my_array.map.with_index do |_num, i|
+      return my_array unless i != my_array.length - 1
       if my_array[i] > my_array[i + 1]
         my_array[i], my_array[i + 1] = my_array[i + 1], my_array[i]
         break
@@ -16,24 +14,23 @@ end
 ar = [4, 3, 78, 2, 98, 0, 2, 1]
 p bubble_sort(ar)
 
-def bubble_sort_by(arr_str)
-  my_array2 = arr_str
-  n = arr_str.length - 1
-  puts n
+def sort_by(array2)
   loop do
-    my_array2.map.with_index do |str, i|
-      puts "value #{str} the index is #{i}"
-      return my_array2 unless i != n
-      if my_array2[i].length > my_array2[i + 1].length
-        my_array2[i], my_array2[i + 1] = my_array2[i + 1], my_array2[i]
+    array2.map.with_index do |_str, i|
+      return array2 unless i != array2.length - 1
+      if array2[i].length > array2[i + 1].length
+        array2[i], array2[i + 1] = array2[i + 1], array2[i]
         break
       end
     end
   end
 end
+
 ar2 = %w[hello r hellomyfriends hey hi]
-result = bubble_sort_by(ar2) do |left, right|
+result = sort_by(ar2) do |left, right|
   left.length - right.length
 end
 
 p result
+
+# rubocop:enable Metrics/AbcSize
